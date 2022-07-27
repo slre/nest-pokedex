f43327bbc5e6f4a57091a77bf67fc39997988b56
tecelotl <tecelotl@tecelotls-MacBook-Pro.local>
2022-07-27 14:57:07 -0500

    Get pokemon by name, id or mongoId

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,19 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { isValidObjectId, Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel : Model<Pokemon>
+  ){}
+
+
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
+    try{
+
+      const pokemon = await this.pokemonModel.create( createPokemonDto );
+      return pokemon;
+
+    }catch(error){
+      if (error.code ===11000){
+        throw new BadRequestException(`Pokemon ${ JSON.stringify( error.keyValue)} already exists`)
+      }
+
+      //console.log(error)
+      throw new InternalServerErrorException(`Cant create pokemon , Check server logs`);
+    }
+      
   }
 
   findAll() {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+    let pokemon : Pokemon;
+    if( !isNaN(+term) ){ // If is Number
+      pokemon = await this.pokemonModel.findOne({ no:term });
+
+    }
+    //MongoID
+    if( !pokemon && isValidObjectId( term ) ){
+      pokemon = await this.pokemonModel.findById( term );
+    }
+
+    // Name
+    if( !pokemon ){
+      pokemon = await this.pokemonModel.findOne({ name:term.toLocaleLowerCase().trim() });
+    }
+
+    // Not found
+    if( !pokemon ){
+      throw new NotFoundException(`Pokemon ${ term } not found `);
+    }
+    return pokemon;
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
